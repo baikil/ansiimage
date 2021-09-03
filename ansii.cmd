6b6b7a979f22cd80b075591eaebfa0294ff6e200
baikil <78218286+baikil@users.noreply.github.com>
2021-09-02 23:01:39 -0400

    Add files via upload

---
+++ ansii.cmd
@@ -0,0 +1,15 @@
+@echo off
+title Ansii  %1
+:0
+echo [0m
+cls
+set /p file=<%1
+set "image=echo %file:[=[48;5;%"
+set "image=%image:]=m%"
+set "image=%image:(=[38;5;%"
+set "image=%image:)=m%"
+set "image=%image:+=&echo %"
+set "image=%image:-=  %"
+%image%
+timeout /t -1 >nul
+goto 0
